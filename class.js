b5026037dfb59899bc455d306941e55b03f206da
ysyoon <yys1423@naver.com>
2022-11-07 16:08:47 +0900

    second commit

---
+++ class.js
@@ -0,0 +1,110 @@
+'use strict';
+// Object-oriendted programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    // constructor
+    constructor(name, age) {
+        // fields
+        this.name = name;
+        this.age = age;
+    }
+
+
+// methods
+speak() {
+    console.log(`${this.name}: hello!`);
+    }
+}
+
+const seory = new Person('seory', 30);
+console.log(seory.name);
+console.log(seory.age);
+seory.speak();
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0) {
+        //    throw Error('age can not be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('steve', 'job', -1);
+console.log(user1.age);
+
+// 3. Fields (public, private)
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log(Article.publisher);
+    }
+}
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+// 6. Class checking: instanceof
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Objects);
